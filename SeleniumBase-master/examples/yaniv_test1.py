25cb3533b4146c98863507e03c98c7ef38252d3c
yanivli <yaniv1984@gmail.com>
2020-08-28 22:38:37 +0300

    first commit

---
+++ SeleniumBase-master/examples/yaniv_test1.py
@@ -0,0 +1,16 @@
+"""
+Same as my_first_test.py, but without the asserts.
+"""
+
+from seleniumbase import BaseCase
+
+
+class MyTestClass(BaseCase):
+
+    def test_basic(self):
+        self.open("https://xkcd.com/353/")
+        self.click('a[rel="license"]')
+        self.go_back()
+        self.click("link=About")
+        self.open("://store.xkcd.com/collections/everything")
+        self.update_text("input.search-input", "xkcd book\n")
